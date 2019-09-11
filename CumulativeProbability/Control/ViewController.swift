b1af170bf41e3716e17950605934f7215eeb2566
James <mistcurve@outlook.com>
2019-09-11 15:25:57 -0400

    Class Additions

--- CumulativeProbability/Control/ViewController.swift
+++ CumulativeProbability/Control/ViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
